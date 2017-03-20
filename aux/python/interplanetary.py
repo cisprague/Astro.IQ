153e7f42366a8590450fe411e0a5a034401d90d6
CISprague <christopher.iliffe.sprague@gmail.com>
2017-03-19 23:28:21 -0400

    Cleaned up the project

--- aux/python/interplanetary.py
+++
@@ -1,5 +0,0 @@
-
-
-if __name__ == "__main__":
-    print("hello, I am chris")
-    h = 2
--- aux/work/Example.py
+++
@@ -1,8 +0,0 @@
-from Rocket import Rocket
-from numpy import pi
-
-# Instantiate rocket
-Falcon = Rocket()
-S = Falcon.State[-1]
-C = [5,pi/4,pi/2]
-Falcon.State_Transition(S,C)
