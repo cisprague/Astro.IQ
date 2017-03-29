599893f0cef31abb9ef1854a1c22e0d998c024c9
CISprague <christopher.iliffe.sprague@gmail.com>
2017-03-29 00:22:07 -0400

    Added width and nlayers combinations

--- src/Scripts/Machine_Learning/Train_ixj_MLP.py
+++ src/Scripts/Machine_Learning/Train_ixj_MLP.py
@@ -3,24 +3,25 @@ sys.path.append('../../')
 import numpy as np
 from ML import MLP
 import multiprocessing
+import itertools
 
 ''' -----------------------------------------
-Here we train 4 different neural networks
-architectures in parallel.
->> python Train_20xl_Mars_Far.py
+Here we train various different neural
+network architectures in parallel.
+>> python Train_ixj_MLP.py
 ----------------------------------------- '''
 
-def main(nlayers):
+def main(width, nlayers):
     # Load the data to be regressed
     data = np.load('../../Data/ML/Mars_Far.npy')
     # Designate the input and output indicies
     iin  = [0,1,2,3,4]
     iout = [5,6]
     # Define the structure of the hidden layers
-    layers = [20]*nlayers
+    layers = [width]*nlayers
     # Save the neural net here
-    path = '../../Data/ML/Nets/Mars_Far_20x' + str(nlayers)
-    print path
+    path = '../../Data/ML/Nets/Mars_Far_'
+    path += str(width) + 'x' + str(nlayers)
     # Instantiate the neural network
     net = MLP(path)
     # Build the neural network
@@ -28,7 +29,7 @@ def main(nlayers):
     # Specify the learning rate
     lr = 1e-4
     # Number of training iterations
-    tit = 300000
+    tit = 200000
     # How often to display cost
     dispr = 300
     # Train the network
@@ -39,6 +40,7 @@ def main(nlayers):
 
 if __name__ == "__main__":
     nlayers = [1, 2, 3, 4, 5]
-    for i in nlayers:
-        p = multiprocessing.Process(target=main, args=(i,))
+    width   = [10, 20]
+    for shape in itertools.product(width, nlayers):
+        p = multiprocessing.Process(target=main, args=shape)
         p.start()
